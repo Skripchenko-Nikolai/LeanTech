cd76a219b3005a1ac82c7b320a13dd356e6cead7
Skripchenko-Nikolai <Skripchenko-Nikolai>
2022-06-09 13:38:21 +0600

    [-]: Сделал экран заполнения профиля, добавил навигацию.

--- app/src/main/java/com/pirksni/leantech/di/module/FragmentModule.kt
+++ app/src/main/java/com/pirksni/leantech/di/module/FragmentModule.kt
@@ -1,11 +1,16 @@
 package com.pirksni.leantech.di.module
 
+import com.pirksni.leantech.presentation.screen.filledprofile.FilledProfileFragment
 import com.pirksni.leantech.presentation.screen.menu.MenuFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface FragmentModule {
+
+    @ContributesAndroidInjector
+    fun provideFilledProfileFragment(): FilledProfileFragment
+
     @ContributesAndroidInjector
     fun provideMenuFragment(): MenuFragment
 }
